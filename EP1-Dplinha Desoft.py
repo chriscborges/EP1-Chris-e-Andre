728e16b34cb96c42eec8329a9496d89b43c3c71d
adambry <38053861+adambry@users.noreply.github.com>
2018-04-24 22:22:05 -0300

    Update EP1-Dplinha Desoft.py

--- EP1-Dplinha Desoft.py
+++ EP1-Dplinha Desoft.py
@@ -3,6 +3,7 @@ import json
 with open('estoque.json','r') as arquivo:
     e = json.loads(arquivo.read())
     
+    
 escolha = -1
 estoque = e
 while escolha !=0:
@@ -13,6 +14,7 @@ while escolha !=0:
     print('2 - Remover item')
     print('3 - Alterar item')
     print('4 - Imprimir estoque')   
+    print('5 - Imprimir valor monetário total')
         
     escolha = int(input('Faça uma escolha: '))
         
@@ -23,15 +25,16 @@ while escolha !=0:
         item = input('Nome do produto: ')
         if item not in estoque:
             quantidade_inicial = int(input('Quantidade inicial: '))
-            preco_unitario = float(input("preço unitario do produto:"))
-            estoque[item] = {'quantidade': quantidade_inicial }
             while quantidade_inicial<0:
                 print('A quantidade inicial não pode ser negativa')
                 quantidade_inicial = int(input('Quantidade inicial: '))
-            estoque[item]['quantidade'] = quantidade_inicial
+            preco_unitario = float(input("preço unitario do produto:"))
             while preco_unitario < 0 :
-                print("o preço do produto não pode ser negativo")
+                print("O preço do produto não pode ser negativo")
                 preco_unitario = float(input('preço unitario do produto: '))
+            estoque[item] = {'quantidade': quantidade_inicial }
+            estoque[item]['quantidade'] = quantidade_inicial
+           
             estoque[item]["preco_unitario"] = preco_unitario
 
         else:
@@ -46,12 +49,12 @@ while escolha !=0:
             
     if escolha == 3:
         item_altera = input('Nome do produto: ')
-        print("digite A para alterar quantidade:")
-        print("digite B para alterar valor:")
-        mudanca = input("qual a alteracao?:")
         if item_altera not in estoque:
             print('Elemento não encontrado')
         else:
+            print("Digite A para alterar quantidade:")
+            print("Digite B para alterar valor")
+            mudanca = input("Qual a alteração?:")
             if mudanca == "A" or mudanca == "a":
                 quantidade = int(input('Quantidade: '))
                 estoque[item_altera]['quantidade'] += quantidade
@@ -60,13 +63,17 @@ while escolha !=0:
                 novo_preco = float(input("novo preco:"))
                 estoque[item_altera]["preco_unitario"] = novo_preco
                 print ("Preço atualizado de {0}: {1}".format(item_altera,estoque[item_altera]["preco_unitario"]))
-
-
-                
-
-            
     if escolha == 4:
-        print(estoque)
+        print(estoque) 
+    if escolha == 5:
+        elementos = []
+        for i in estoque:
+            valor_tot = estoque[i]['quantidade']*estoque[i]['preco_unitario']
+            elementos.append(valor_tot)
+        resultado = sum(elementos)
+        print('O valor monetário total é: {}'.format(resultado))
+                
+            
             
 estoque_json = json.dumps(estoque, sort_keys = True, indent = 4)           
         
