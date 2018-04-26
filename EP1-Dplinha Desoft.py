1826a089e4c9e9cb435390d2de2db27004b1f69a
adambry <38053861+adambry@users.noreply.github.com>
2018-04-26 18:18:13 -0300

    Update EP1-Dplinha Desoft.py

--- EP1-Dplinha Desoft.py
+++ EP1-Dplinha Desoft.py
@@ -3,9 +3,16 @@ import json
 with open('estoque.json','r') as arquivo:
     e = json.loads(arquivo.read())
     
-    
 escolha = -1
 estoque = e
+a = estoque.keys()
+
+if len(estoque)!=0:
+    nome_produtos = []
+    for r in a:
+        nome_produtos.append(r)
+
+
 while escolha !=0:
     print('\n')
     print('Controle de estoque')
@@ -63,15 +70,40 @@ while escolha !=0:
                 novo_preco = float(input("novo preco:"))
                 estoque[item_altera]["preco_unitario"] = novo_preco
                 print ("Preço atualizado de {0}: {1}".format(item_altera,estoque[item_altera]["preco_unitario"]))
+         
     if escolha == 4:
-        print(estoque) 
+        estoques_negativos   = []
+        estoques_positivos   = []
+        x = 0
+        for b in estoque:
+            if estoque[b]['quantidade'] < 0:
+                estoques_negativos.append(nome_produtos[x])
+            elif estoque[b]['quantidade'] >= 0:
+                estoques_positivos.append(nome_produtos[x])
+            x+=1
+        z = 0
+        print('Produtos com quantidades positivas:')
+        if len(estoques_positivos) !=0: 
+            for k in estoque:
+                if k in estoques_positivos:
+                    print('\n {0}:\n    Preço unitário: {1}\n    Quantidade: {2}\n'.format(estoques_positivos[z],estoque[k]['preco_unitario'],estoque[k]['quantidade']))
+                z+=1
+        t=0
+        print('Produtos com quantidades negativas:')
+        if len(estoques_negativos) != 0:
+            for l in estoque:
+                if l in estoques_negativos:
+                    print('\n{0}:\n     Preço unitário: {1}\n    Quantidade: {2}\n'.format(estoques_negativos[t],estoque[l]['preco_unitario'],estoque[l]['quantidade']))
+                    t+=1
+        
     if escolha == 5:
         elementos = []
         for i in estoque:
             valor_tot = estoque[i]['quantidade']*estoque[i]['preco_unitario']
-            elementos.append(valor_tot)
+            if valor_tot > 0:
+                elementos.append(valor_tot)
         resultado = sum(elementos)
-        print('O valor monetário total é: {}'.format(resultado))
+        print('\n O valor monetário total é: {}'.format(resultado))
                 
             
             
@@ -79,5 +111,3 @@ estoque_json = json.dumps(estoque, sort_keys = True, indent = 4)
         
 with open('estoque.json','w') as saida:
     saida.write(estoque_json)
-    
-    
